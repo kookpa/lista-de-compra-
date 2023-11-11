78e549c0144eb6405a86df18ba864ff89af4f055
kookpa <141832427+kookpa@users.noreply.github.com>
2023-11-11 13:50:17 -0300

    Add files via upload

---
+++ atividades/lista_de_compras/lista_de_compras/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace lista_de_compras
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txblista = new System.Windows.Forms.TextBox();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.btnCompra = new System.Windows.Forms.Button();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txblista
+            // 
+            this.txblista.Location = new System.Drawing.Point(111, 79);
+            this.txblista.Name = "txblista";
+            this.txblista.Size = new System.Drawing.Size(132, 20);
+            this.txblista.TabIndex = 0;
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Location = new System.Drawing.Point(97, 130);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(75, 51);
+            this.btnRemover.TabIndex = 1;
+            this.btnRemover.Text = "Remover";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // btnCompra
+            // 
+            this.btnCompra.Location = new System.Drawing.Point(212, 129);
+            this.btnCompra.Name = "btnCompra";
+            this.btnCompra.Size = new System.Drawing.Size(71, 51);
+            this.btnCompra.TabIndex = 2;
+            this.btnCompra.Text = "Compra ";
+            this.btnCompra.UseVisualStyleBackColor = true;
+            this.btnCompra.Click += new System.EventHandler(this.btnCompra_Click);
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(154, 205);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(67, 52);
+            this.btnAdicionar.TabIndex = 3;
+            this.btnAdicionar.Text = "Add";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(332, 89);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(118, 134);
+            this.listBox.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.btnCompra);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.txblista);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txblista;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.Button btnCompra;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.ListBox listBox;
+    }
+}
+
---
+++ atividades/lista_de_compras/lista_de_compras/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace lista_de_compras
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            // Adiciona o item digitado na caixa de texto à lista
+            string item = txblista.Text;
+            if (!string.IsNullOrEmpty(item))
+            {
+                listBox.Items.Add(item);
+                txblista.Clear();
+            }
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            // Remove o item selecionado da lista
+            int index = listBox.SelectedIndex;
+            if (index != -1)
+            {
+                listBox.Items.RemoveAt(index);
+            }
+        }
+
+        private void btnCompra_Click(object sender, EventArgs e)
+        {
+            // Marca o item selecionado como comprado
+            int index = listBox.SelectedIndex;
+            if (index != -1)
+            {
+                string item = listBox.Items[index].ToString();
+                if (!item.StartsWith("[x] "))
+                {
+                    item = "[x] " + item;
+                    listBox.Items[index] = item;
+                }
+            }
+        }
+    }
+}
